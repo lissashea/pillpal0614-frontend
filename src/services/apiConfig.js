6de7dd003ed47e5dd32c5bc3b4498db511bd043c
“Lissa <lwarshaw674@gmail.com>
2023-06-24 11:48:17 -0400

    src

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:8000';
+const BASE_URL = process.env.REACT_APP_BASE_URL ;
+// || 'http://localhost:8000';
 
 export async function fetchProfileData(token) {
   try {
